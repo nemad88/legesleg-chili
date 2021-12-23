8c8f684b30012188c7273ebda38e8dad0ef15ed7
Adam Nemeth <nemethadam88@gmail.com>
2021-12-23 16:04:51 +0100

    fullpage image fix

--- src/components/FullPageImage/FullPageImage.style.ts
+++ src/components/FullPageImage/FullPageImage.style.ts
@@ -2,8 +2,8 @@ import styled from "styled-components";
 import aCsapatJPG from "../../assets/a_csapat.jpg";
 
 export const StyledFullPageImage = styled.div`
-  width: 100%;
-  height: 1020px;
+  width: 100vw;
+  height: 100vh;
   background-image: url(${aCsapatJPG});
   background-repeat: no-repeat;
   background-attachment: fixed;
